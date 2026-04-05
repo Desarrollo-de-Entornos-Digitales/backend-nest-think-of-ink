7b7ad8a97c06fba4b6f40908d71ef3a7966fd485
Alejandro Acosta <alejo.acosta.q@gmail.com>
2026-04-05 12:31:17 -0500

    commits done in order to function correctely

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Role } from './role.entity';
-import { RolesService } from './roles.service';
-import { RolesController } from './roles.controller';
+import { roleService } from './roles.service';
+import { RoleController } from './roles.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Role])],
-  providers: [RolesService],
-  controllers: [RolesController],
+  providers: [roleService],
+  controllers: [RoleController],
 })
 export class RolesModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Role } from '../roles/role.entity';
-import { UsersController } from './users.controller';
+import { UserController } from './users.controller';
 import { UsersService } from './users.service.spec';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Role])],
   providers: [UsersService],
-  controllers: [UsersController],
+  controllers: [UserController],
 })
 export class UsersModule {}
